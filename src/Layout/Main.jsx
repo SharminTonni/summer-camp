8d7cdde431dcf91c08b37716b83b552bb5c79212
SharminTonni <sharmintonni000@gmail.com>
2023-06-14 20:35:56 +0600

    theme toggle done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,29 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "../Shared/Navigation";
 import Footer from "../Shared/Footer";
+import { useEffect, useState } from "react";
 
 const Main = () => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+  const handleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
   return (
-    <div>
+    <div className="dark:bg-black">
+      <button
+        onClick={handleTheme}
+        className="btn text-center dark:text-black mb-5 lg:ml-40 mt-10 text-white btn-ghost h-10 dark:bg-white bg-black "
+      >
+        Switch to Dark Mode
+      </button>
       <Navigation></Navigation>
       <Outlet></Outlet>
       <Footer></Footer>
